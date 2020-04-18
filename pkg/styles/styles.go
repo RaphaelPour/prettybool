bc7011cf7f25d0ae0fa53ae2a66771ae8af95145
Raphael Pour <info@raphaelpour.de>
2020-04-18 15:15:29 +0200

    Styles: Add mising styles and list method

--- pkg/styles/styles.go
+++ pkg/styles/styles.go
@@ -10,8 +10,14 @@ type style struct {
 
 var (
 	styles = map[string]style{
-		"ok":  {ok: "ok", notOk: "not ok"},
-		"yes": {ok: "yes", notOk: "no"},
+		"ok":       {ok: "ok", notOk: "not ok"},
+		"yes":      {ok: "yes", notOk: "no"},
+		"true":     {ok: "true", notOk: "false"},
+		"0":        {ok: "0", notOk: "1"},
+		"passed":   {ok: "passed", notOk: "failed"},
+		"positive": {ok: "positive", notOk: "negative"},
+		"good":     {ok: "good", notOk: "bad"},
+		"check":    {ok: "\u2705", notOk: "\u274c"},
 	}
 )
 
@@ -28,4 +34,12 @@ func GetPrettyBool(value bool, styleName string) (string, error) {
 	}
 
 	return s.notOk, nil
+}
+
+func ListStyles() {
+	fmt.Println("Pretty Boolean styles:")
+
+	for name, s := range styles {
+		fmt.Printf("%s: %s, %s\n", name, s.ok, s.notOk)
+	}
 }
